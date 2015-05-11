b123d7ffc1fb99a9e729cc82b83524f3b400ec1c
corvus <corvus@users.noreply.github.com>
2015-05-12 00:08:38 +0200

    added logging

--- script.php
+++ script.php
@@ -5,108 +5,161 @@
 /*         http://www.zweischneider.de          */
 /* ******************************************** */
 
-if(!empty($_REQUEST['url']) && !empty($_REQUEST['security'])) {
-    date_default_timezone_set('UTC');
+const LOG_ENABLED = false; // true enables logging of incoming requests
+const LOG_MAX_FILESIZE = 1024; // in KB
+const LOG_MAX_FILES = 7;
+
+/* ##################################################################################################### */
+/* dont change anything below this line                                                                  */
+/* ##################################################################################################### */
+
+date_default_timezone_set('UTC');
+
+$log = array();
+$log[] = date('c');
+
+if (!empty($_REQUEST['url']) && !empty($_REQUEST['security'])) {
+    $log[] = 'INFO: $_REQUEST';
+    $log[] = trim(print_r($_REQUEST, true));
 
     $check = false;
-    for($i=time(); $i>=(time()-15); $i--) {
-        if($_REQUEST['security'] == md5($i.'%cryptethash%')) {
+    for ($i=time(); $i>=(time()-15); $i--) {
+        if ($_REQUEST['security'] == md5($i.'%cryptethash%')) {
             $check = true;
             break;
         }
     }
 
-    if(!$check) {
-        header('HTTP/1.0 404 Not Found');
-        die;
-    }
+    if (!$check) {
+        $log[] = 'ERROR: Security check failed';
+        header((isset($_SERVER["SERVER_PROTOCOL"]) ? $_SERVER["SERVER_PROTOCOL"] : 'HTTP/1.0') . ' 404 Not Found');
+    } else {
+        // GET CONTENT OF WEBSITE
+        $total = 0;
+        $start = microtime(true);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $_REQUEST['url']);
+        curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $content = curl_exec($ch);
+        $getInfo = curl_getinfo($ch);
+        curl_close($ch);
 
-    // GET CONTENT OF WEBSITE
-    $total = 0;
-    $start = microtime(true);
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $_REQUEST['url']);
-    curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    $content = curl_exec($ch);
-    $getInfo = curl_getinfo($ch);
-    curl_close($ch);
+        $return['http_code'] = $getInfo['http_code'];
 
-    $return['http_code'] = $getInfo['http_code'];
-
-    // LOAD ALL SOURCES FROM WEBSITE
-    preg_match_all('/src=(["\']?)([^\1]+?)\1/m', $content, $result, PREG_PATTERN_ORDER);
-    $result = $result[2];
-    preg_match_all('/background(-image)??\s*?:.*?url\(["|\']??(.+)["|\']??\)/', $content, $result2, PREG_PATTERN_ORDER);
-    foreach ($result2[2] AS $val) {
-        $result[] = $val;
-    }
-    $headers = array(
-        'Cache-Control: no-cache',
-    );
-    do {
-        $init_array = array();
-        $mh = curl_multi_init();
-        $count = 0;
-        foreach ($result as $key => $src) {
-            if (strpos($src, '//') === false) {
-                $url = $_REQUEST['url'] . $src;
-            } else {
-                $url = $src;
-            }
-            if (strpos($url, '//') == 0) {
-                $url = 'http:' . $url;
-            }
-            $init_array[$key] = curl_init();
-            curl_setopt($init_array[$key], CURLOPT_URL, $url);
-            curl_setopt($init_array[$key], CURLOPT_FRESH_CONNECT, true);
-            curl_setopt($init_array[$key], CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($init_array[$key], CURLOPT_HTTPHEADER, $headers);
-            curl_setopt($init_array[$key], CURLOPT_CONNECTTIMEOUT, 30);
-            curl_setopt($init_array[$key], CURLOPT_TIMEOUT, 30);
-            curl_multi_add_handle($mh, $init_array[$key]);
-            unset($result[$key]);
-            $count++;
-            if ($count > 9) {
-                break;
-            }
+        // LOAD ALL SOURCES FROM WEBSITE
+        preg_match_all('/src=(["\']?)([^\1]+?)\1/m', $content, $result, PREG_PATTERN_ORDER);
+        $result = $result[2];
+        preg_match_all('/background(-image)??\s*?:.*?url\(["|\']??(.+)["|\']??\)/', $content, $result2, PREG_PATTERN_ORDER);
+        foreach ($result2[2] AS $val) {
+            $result[] = $val;
         }
-
-        $running = null;
+        $headers = array(
+            'Cache-Control: no-cache',
+        );
         do {
-            curl_multi_exec($mh, $running);
-        } while ($running > 0);
+            $init_array = array();
+            $mh = curl_multi_init();
+            $count = 0;
+            foreach ($result as $key => $src) {
+                if (strpos($src, '//') === false) {
+                    $url = $_REQUEST['url'] . $src;
+                } else {
+                    $url = $src;
+                }
+                if (strpos($url, '//') == 0) {
+                    $url = 'http:' . $url;
+                }
+                $init_array[$key] = curl_init();
+                curl_setopt($init_array[$key], CURLOPT_URL, $url);
+                curl_setopt($init_array[$key], CURLOPT_FRESH_CONNECT, true);
+                curl_setopt($init_array[$key], CURLOPT_RETURNTRANSFER, true);
+                curl_setopt($init_array[$key], CURLOPT_HTTPHEADER, $headers);
+                curl_setopt($init_array[$key], CURLOPT_CONNECTTIMEOUT, 30);
+                curl_setopt($init_array[$key], CURLOPT_TIMEOUT, 30);
+                curl_multi_add_handle($mh, $init_array[$key]);
+                unset($result[$key]);
+                $count++;
+                if ($count > 9) {
+                    break;
+                }
+            }
 
-        foreach ($init_array AS $key => $val) {
-            curl_multi_remove_handle($mh, $init_array[$key]);
-        }
-        curl_multi_close($mh);
-    } while (count($result) > 0);
-    $total = microtime(true) - $start;
-    $return['complete_loading_time'] = $total;
+            $running = null;
+            do {
+                curl_multi_exec($mh, $running);
+            } while ($running > 0);
 
-    // SEND NOTIFICATION TO CROWDPING
-    $return['hash'] = $_REQUEST['hash'];
-    $return['version'] = '0.2';
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLOPT_HEADER, 0);
-    curl_setopt($ch, CURLOPT_URL, 'https://api.crowdping.net/1.0/load_times');
-    curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
-    curl_setopt($ch, CURLOPT_NOSIGNAL, 1);
-    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
-    curl_setopt($ch, CURLOPT_TIMEOUT, 1);
-    $headerArray = array('Content-Type: application/json');
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headerArray);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($return));
-    curl_exec($ch);
-    curl_close($ch);
+            foreach ($init_array AS $key => $val) {
+                curl_multi_remove_handle($mh, $init_array[$key]);
+            }
+            curl_multi_close($mh);
+        } while (count($result) > 0);
+        $total = microtime(true) - $start;
+        $return['complete_loading_time'] = $total;
 
-    echo 'crowdping_script';
+        // SEND NOTIFICATION TO CROWDPING
+        $return['hash'] = $_REQUEST['hash'];
+        $return['version'] = '0.2';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_URL, 'https://api.crowdping.net/1.0/load_times');
+        curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
+        curl_setopt($ch, CURLOPT_NOSIGNAL, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 1);
+        $headerArray = array('Content-Type: application/json');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headerArray);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($return));
+        curl_exec($ch);
+        curl_close($ch);
+
+        $log[] = 'INFO: success';
+
+        echo 'crowdping_script';
+    }
 } else {
-    header('HTTP/1.0 404 Not Found');
-    die;
+    header((isset($_SERVER["SERVER_PROTOCOL"]) ? $_SERVER["SERVER_PROTOCOL"] : 'HTTP/1.0') . ' 404 Not Found');
+}
+
+if (LOG_ENABLED === true && count($log) > 1) {
+    $logFile = __FILE__ . '.log';
+    $fp = @fopen($logFile, 'a');
+
+    if ($fp !== false && @flock($fp, LOCK_EX)) {
+        $log[] = '###############################################################';
+        $logContent = implode(PHP_EOL, $log) . PHP_EOL;
+
+        if (@filesize($logFile) > LOG_MAX_FILESIZE * 1024) {
+            // rotate log files
+            for ($i = LOG_MAX_FILES; $i > 0; --$i) {
+                $rotateFile = $logFile . '.' . $i;
+                if (is_file($rotateFile)) {
+                    if ($i === LOG_MAX_FILES) {
+                        @unlink($rotateFile);
+                    }
+                    else {
+                        @rename($rotateFile, $logFile . '.' . ($i + 1));
+                    }
+                }
+            }
+
+            if (is_file($logFile)) {
+                @rename($logFile, $logFile . '.1');
+            }
+
+            @flock($fp, LOCK_UN);
+            @fclose($fp);
+            @file_put_contents($logFile, $logContent, FILE_APPEND | LOCK_EX);
+        }
+        else {
+            fwrite($fp, $logContent);
+            flock($fp, LOCK_UN);
+            @fclose($fp);
+        }
+    }
 }
